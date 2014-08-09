b001081c36b506a3f2d8c669652ead942dd179a1
Gerrit Sommer <gerrit.sommer@outlook.com>
2014-08-09 18:59:14 +0200

    init with user

---
+++ app/router.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: EmberappENV.locationType
+});
+
+Router.map(function() {
+  this.resource('users', function() {
+    this.route('new');
+    this.resource('user', {
+      path: '/:user_id'
+    }, function() {
+      this.route('edit');
+      this.route('details');
+    });
+  });
+});
+
+export default Router;
+
+
+
---
+++ app/routes/users.js
@@ -0,0 +1,8 @@
+// routes/users.js
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return this.store.find('user');
+  }
+});
